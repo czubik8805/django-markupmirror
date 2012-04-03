25f8bf8bddb3fe10cb493b541ed4f87bd74ba52f
Fabian B?chler <fabian.buechler@gmail.com>
2012-04-03 22:21:56 +0200

    Added some improvements.

--- markupmirror/urls.py
+++ markupmirror/urls.py
@@ -2,13 +2,11 @@ from django.conf.urls import patterns
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
 
-from wienfluss.models.pagecontent.markdown.models import MarkDownContentPreview
+from markupmirror.views import MarkupPreview
 
 
 urlpatterns = patterns('',
-    url(r'^preview/$',
-        MarkDownContentPreview.as_view(),
-        name='preview'),
+    url(r'^preview/$', MarkupPreview.as_view(), name='preview'),
     url(r'^base/$',
         TemplateView.as_view(
             template_name='markupmirror/preview.html'),
