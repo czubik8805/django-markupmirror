23f6e8071920c75943ebea43bf757fc12b89f6f1
Fabian B?chler <fabian.buechler@gmail.com>
2012-04-06 13:51:07 +0200

    Updated markup definition modules to avoid naming conflicts with the python implementations of the corresponding markups.

--- markupmirror/markup/__init__.py
+++ markupmirror/markup/__init__.py
@@ -1,7 +1,7 @@
 # Add all markup.* submodules here for autodiscovery
 from markupmirror.markup.base import *
-from markupmirror.markup.plain import *
 from markupmirror.markup.html import *
-from markupmirror.markup.md import *
-from markupmirror.markup.rest import *
-from markupmirror.markup.textile import *
+from markupmirror.markup.markdown_ import *
+from markupmirror.markup.plaintext import *
+from markupmirror.markup.restructuredtext import *
+from markupmirror.markup.textile_ import *
