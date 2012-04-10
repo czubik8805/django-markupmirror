7977027ad894b88b0d2911039f8af681ddfab302
Fabian B?chler <buechler@wienfluss.net>
2012-04-10 16:58:38 +0200

    Trying to initialize widget attributes for CodeMirror initialization from form or widget definition.

--- markupmirror/widgets.py
+++ markupmirror/widgets.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.admin.widgets import AdminTextareaWidget
 
+from markupmirror import settings
+from markupmirror.markup.base import markup_pool
+
 
 class MarkupMirrorTextarea(forms.Textarea):
 
@@ -9,10 +12,35 @@ class MarkupMirrorTextarea(forms.Textarea):
             value = value.raw
         return super(MarkupMirrorTextarea, self).render(name, value, attrs)
 
+    class Media:
+        css = {
+            'all': settings.MARKUPMIRROR_CSS
+        }
+        js = settings.MARKUPMIRROR_JS
+
 
 class AdminMarkupMirrorTextareaWidget(
     MarkupMirrorTextarea, AdminTextareaWidget):
-    pass
+
+    def render(self, name, value, attrs=None):
+        """Adds attributes necessary to load CodeMirror for each textarea
+        in the admin.
+
+        """
+        if value and hasattr(value, 'markup_type'):
+            # get markup converter by type.
+            # ``value`` is ``markupmirror.fields.Markup``.
+            markup_type = value.markup_type
+            markup = markup_pool.get_markup(markup_type)
+
+            default_attrs = {
+                'data-mode': markup.codemirror_mode,
+                'data-markuptype': markup_type,
+            }
+            default_attrs.update(attrs)
+
+        return super(AdminMarkupMirrorTextareaWidget, self).render(
+            name, value, attrs)
 
 
 __all__ = ('MarkupMirrorTextarea', 'AdminMarkupMirrorTextareaWidget')
