eac3b985fb40e7f6a18787c170face57ba94a5da
Fabian B?chler <buechler@wienfluss.net>
2012-05-03 13:49:49 +0200

    Streamlined JS code.

--- markupmirror/static/markupmirror/markupmirror.js
+++ markupmirror/static/markupmirror/markupmirror.js
@@ -2,128 +2,145 @@ var markupmirror = {
     "jQuery": jQuery
 };
 
-(function($) {
-    $(function() {
+/* document ready and jQuery closure */
+(function($) { $(function() {
 
-        var MarkupMirrorEditor = function( element, options ) {
-                    var _this = this,
-                        _public = {
 
-                            /* to configure the wrapper */
-                            configure: function( obj ) {
-                                for( key in obj ) {
-                                    _private.options[ key ] = obj[ key ];
-                                }
 
-                                return _this;
-                            },
+var MarkupMirrorEditor = function( element, options ) {
+    var _this = this,
+        _public = {
 
-                            get: function( key ) {
-                                return _private.options[ key ] || undefined;
-                            },
+            /* to configure the wrapper */
+            configure: function( obj ) {
+                var key;
+                for( key in obj ) {
+                    _private.options[ key ] = obj[ key ];
+                }
 
-                            /* add the Codemirror to a new element */
-                            add: function( el, options ) {
-                                    if( !el || el.length === 0 ) {
-                                        return _this;
-                                    }
+                return _this;
+            },
 
-                                    var editor;
+            get: function( key ) {
+                return _private.options[ key ] || undefined;
+            },
 
-                                     $.each( el,
-                                            function() {
-                                                if( this.nodeName.toLowerCase() === 'textarea' ) {
-                                                    if( options === undefined ) {
-                                                        if( _private.getOption( '_init' ) === true && _private.getOption( 'inherit' ) === true ) {
-                                                            editor = CodeMirror.fromTextArea( this, _private.getOption( 'initOptions' ) );
-                                                            _public.configure( { 'editor': editor } );
-                                                        } else {
-                                                            editor = CodeMirror.fromTextArea( this );
-                                                            _public.configure( { 'editor': editor } );
-                                                        }
-                                                    } else {
+            /* add the Codemirror to a new element */
+            add: function( el, options ) {
+                if( !el || el.length === 0 ) {
+                    return _this;
+                }
 
-                                                        /* so we are able to overwrite just a few of the options */
-                                                        if(  _private.getOption( '_init' ) === true && _private.getOption( 'extend' ) ) {
-                                                            options = $.extend( _private.getOption( 'initOptions' ), options );
-                                                        }
-
-                                                        editor = CodeMirror.fromTextArea( this, options );
-                                                        _public.configure( { 'editor': editor } );
-                                                    }
-
-                                                    if( typeof( _private.getOption( 'onInit' ) ) === 'function' ) {
-                                                         _private.getOption( 'onInit' )( this );
-                                                    }
-                                                }
-                                            });
-
-                                    return _this;
-                                 }
-                        },
-
-                        /* this will not be exposed to the public */
-                        _private = {
-                            getOption: function( index ) {
-                                return _private.options[ index ] || undefined;
-                            },
-
-                            /* store configuration options here */
-                            options: {
-                                /* initial options passed in */
-                                initOptions: undefined,
-
-                                /* new added elements inherit from the inital options */
-                                inherit: true,
-
-                                /* extends the original options object by passing a new element in */
-                                extend: true,
-
-                                /* initial init was done */
-                                _init: false,
-
-                                /* callback */
-                                onInit: undefined,
-
-                                /* the original codemirror editor object */
-                                editor: undefined
+                var editor;
+                $.each( el, function() {
+                    if( this.nodeName.toLowerCase() === 'textarea' ) {
+                        if( options === undefined ) {
+                            if( _private.getOption( '_init' ) === true &&
+                                _private.getOption( 'inherit' ) === true ) {
+                                editor = CodeMirror.fromTextArea(
+                                    this,
+                                    _private.getOption( 'initOptions' ) );
+                                _public.configure( { 'editor': editor } );
+                            } else {
+                                editor = CodeMirror.fromTextArea( this );
+                                _public.configure( { 'editor': editor } );
                             }
-                        };
+                        } else {
 
-                    /* initialise the plugin here */
-                    _public.configure( { _init: true,
-                                         initOptions: options } );
+                            /* so we are able to overwrite just a few of the
+                             * options */
+                            if( _private.getOption( '_init' ) === true &&
+                                _private.getOption( 'extend' ) ) {
+                                options = $.extend(
+                                    _private.getOption( 'initOptions' ),
+                                    options );
+                            }
 
-                    /* add init element */
-                    _public.add( element, options );
+                            editor = CodeMirror.fromTextArea( this, options );
+                            _public.configure( { 'editor': editor } );
+                        }
 
-                    return _this = _public;
-                };
+                        if( typeof(
+                            _private.getOption( 'onInit' ) ) === 'function' ) {
+                            _private.getOption( 'onInit' )( this );
+                        }
+                    }
+                });
+
+                return _this;
+            }
+        },
+
+        /* this will not be exposed to the public */
+        _private = {
+            getOption: function( index ) {
+                return _private.options[ index ] || undefined;
+            },
+
+            /* store configuration options here */
+            options: {
+                /* initial options passed in */
+                initOptions: undefined,
+
+                /* new added elements inherit from the inital
+                 * options */
+                inherit: true,
+
+                /* extends the original options object by passing a
+                 * new element in */
+                extend: true,
+
+                /* initial init was done */
+                _init: false,
+
+                /* callback */
+                onInit: undefined,
+
+                /* the original codemirror editor object */
+                editor: undefined
+            }
+        };
+
+    /* initialise the plugin here */
+    _public.configure( { _init: true,
+                         initOptions: options } );
+
+    /* add init element */
+    _public.add( element, options );
+
+    return _this = _public;
+};
 
 
-        var $textarea = $('.item-markupmirror'),
-            CM = new MarkupMirrorEditor( undefined, $textarea.data('mmSettings') )
-                        .configure({'onInit': createIframe})
-                        .configure({'onChange': updateIframe})
-                        .add( $textarea );
+/* initialize plugin for all textareas, class item-markupmirror */
+var $textarea = $('.item-markupmirror'),
+    CM = new MarkupMirrorEditor( undefined,
+                                 $textarea.data('mmSettings') )
+        .configure({'onInit': createIframe})
+        .configure({'onChange': updateIframe})
+        .add( $textarea );
 
-                /* when init a textarea with codemirror we also create an iframe */
-                function createIframe( el ) {
-                    var $el = $(el),
-                        $iframe = $('<iframe />').attr('src', $el.data('mmSettings')['base_url']);
 
-                    $el.on('change', updateIframe);
+/* when init a textarea with codemirror we also create an iframe */
+function createIframe( el ) {
+    var $el = $(el),
+        $iframe = $('<iframe />').attr(
+            'src', $el.data('mmSettings')['base_url']);
 
-                    $iframe
-                    .insertAfter( $el );
-                }
+    $el.on('change', updateIframe);
+    $iframe.insertAfter( $el );
+}
 
-                /* when changing the textarea we replace the iframe content with the new coming from the server */
-                function updateIframe( e ) {
-                    console.log('update');
-                }
 
-            console.log( CM.get('editor') );
+/* when changing the textarea we replace the iframe content with the
+ * new coming from the server */
+function updateIframe( e ) {
+    console.log('update');
+}
 
-    });
-})(markupmirror.jQuery);
+console.log( CM.get('editor') );
+
+
+
+/* end document ready and jQuery closure */
+}); })(markupmirror.jQuery);
